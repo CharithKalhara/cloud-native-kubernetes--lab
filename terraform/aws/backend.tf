cf76734631b45a986f096d2d62f967cc3ad38509
CharithKalhara <charithkalhara2005@gmail.com>
2026-06-29 09:00:27 +0530

    Update backend.tf

--- terraform/aws/backend.tf
+++ terraform/aws/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "charith-terraform-state"
+    bucket         = "charith-terraform-state-396608776779-ap-southeast-1-an"
     key            = "cloud-native-kubernetes/terraform.tfstate"
     region         = "ap-southeast-1"
     dynamodb_table = "terraform-locks"
