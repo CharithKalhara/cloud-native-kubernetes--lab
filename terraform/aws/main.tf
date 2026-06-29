2a64f53ec4490d314498de35283f623dd7e2ccee
CharithKalhara <charithkalhara2005@gmail.com>
2026-06-29 08:43:13 +0530

    Update main.tf

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -75,7 +75,7 @@ resource "aws_security_group" "web" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["YOUR_PUBLIC_IP/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     from_port = 443
